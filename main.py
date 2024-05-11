2aef35fa5715a98b95cc65e7584d5ed8b2caa492
ahmed nabil <ahmed.nabil.4426@gmail.com>
2024-05-11 19:55:55 +0300

    1v1 Added

--- main.py
+++ main.py
@@ -2,19 +2,21 @@ from Controller import *
 
 if __name__ == '__main__':
     Controller = controller()
-    last = 1
-    while True:
-        if len(Controller.getValidMoves(last)) == 0:
-            last = (last + 1) % 2
-            continue
-        Controller.display_board()
-        print("valid moves")
-        print(Controller.getValidMoves(last))
-        print(Controller.PlayerScores)
-        print(f"player {last + 1} turn")
-        x = int(input())
-        y = int(input())
-        if not Controller.make_move(x, y, last):
-            print("invalid move")
-            continue
-        last = (last + 1) % 2
+    Controller.player_vs_player()
+
+    # last = 1
+    # while True:
+    #     if len(Controller.getValidMoves(last)) == 0:
+    #         last = (last + 1) % 2
+    #         continue
+    #     Controller.display_board()
+    #     print("valid moves")
+    #     print(Controller.getValidMoves(last))
+    #     print(Controller.PlayerScores)
+    #     print(f"player {last + 1} turn")
+    #     x = int(input())
+    #     y = int(input())
+    #     if not Controller.make_move(x, y, last):
+    #         print("invalid move")
+    #         continue
+    #     last = (last + 1) % 2
